95e75c5c2b26e66b4c74812e4f6ce482a89fd712
xavicrip <wxbustamante@utpl.edu.ec>
2026-04-08 17:33:46 -0500

    uso de clase curso

---
+++ curso.java
@@ -0,0 +1,22 @@
+public class curso {
+    // Atributos
+    private String id;
+    private String paralelo;
+    private int numAlumnos;
+    private String aula;
+    private String lugar;
+
+    // Metodos
+    public void estudiar(){
+        System.out.println("Estudiando...");
+    }
+
+    public void aisitir(){
+        System.out.println("Asistir a clases");
+    }
+
+    public String participar(){
+        return "Participando en el curso";
+    }
+    
+}
---
+++ main.java
@@ -0,0 +1,24 @@
+public class main {
+    public static void main(String[] args) {
+        curso curso1 = new curso();
+        curso1.estudiar();
+        curso1.aisitir();
+        curso1.participar();
+
+        // objeto curso 2
+        curso curso2 = new curso();
+        curso2.estudiar();
+        curso2.aisitir();
+        curso2.participar();    
+
+        // objeto curso 3
+        curso curso3 = new curso();
+        curso3.estudiar();
+        curso3.aisitir();
+        curso3.participar();    
+
+
+
+
+    }
+}
